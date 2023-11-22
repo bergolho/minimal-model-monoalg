61a9da02f9b445a46017ba25469cb193786c9c7d
Jpbanhato <jpbbanhato@gmail.com>
2023-11-22 17:31:20 -0300

    CPU version works

--- src/models_library/minimal_model/minimal_model.cu
+++ src/models_library/minimal_model/minimal_model.cu
@@ -1,26 +1,15 @@
 #include "../../gpu_utils/gpu_utils.h"
 #include "../../monodomain/constants.h"
-#include "tt3_mixed.h"
 #include <stddef.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <math.h>
+#include "minimal_model.h"
+#include <stdio.h>
 
 extern "C" SET_ODE_INITIAL_CONDITIONS_GPU(set_model_initial_conditions_gpu) {
 
-    char *cell_type;
-    #ifdef ENDO
-        cell_type = strdup("ENDO");
-    #endif
-
-    #ifdef EPI
-        cell_type = strdup("EPI");
-    #endif
-
-    #ifdef MCELL
-        cell_type = strdup("MCELL");
-    #endif
-
-    log_info("Using ten Tusscher 3 %s GPU model\n", cell_type);
-
-    free(cell_type);
+    log_info("Using Minimal Model GPU\n");
 
     uint32_t num_volumes = solver->original_num_cells;
 
@@ -32,7 +21,6 @@ extern "C" SET_ODE_INITIAL_CONDITIONS_GPU(set_model_initial_conditions_gpu) {
     check_cuda_error(cudaMallocPitch((void **) &(solver->sv), &pitch_h, size, (size_t )NEQ));
     check_cuda_error(cudaMemcpyToSymbol(pitch, &pitch_h, sizeof(size_t)));
 
-    //kernel_set_model_inital_conditions <<<GRID, BLOCK_SIZE>>>(solver->sv, mapping_device, num_volumes);
     kernel_set_model_inital_conditions <<<GRID, BLOCK_SIZE>>>(solver->sv, NULL, num_volumes);
 
     check_cuda_error( cudaPeekAtLastError() );
@@ -66,58 +54,17 @@ extern "C" SOLVE_MODEL_ODES(solve_model_odes_gpu) {
 
     real *fibrosis_device;
     real *fibs = NULL;
-    int num_extra_parameters = 7;
-    real extra_par[num_extra_parameters];
-
-    size_t extra_parameters_size = num_extra_parameters * sizeof(real);
-
-    real *extra_parameters_device;
     real fibs_size = num_cells_to_solve*sizeof(real);
 
-    //struct extra_data_for_fibrosis* extra_data_from_cpu = (struct extra_data_for_fibrosis*)ode_solver->ode_extra_data;
+    bool deallocate = true;
 
-    bool deallocate = false;
-
-    //if(ode_solver->ode_extra_data) {
-    if(0){
-        //fibs = extra_data_from_cpu->fibrosis;
-        //extra_par[0] = extra_data_from_cpu->atpi;
-        //extra_par[1] = extra_data_from_cpu->Ko;
-        //extra_par[2] = extra_data_from_cpu->Ki;
-        //extra_par[3] = extra_data_from_cpu->Vm_modifier;
-        //extra_par[4] = extra_data_from_cpu->GNa_multiplicator;
-        //extra_par[5] = extra_data_from_cpu->GCaL_multiplicator;
-        //extra_par[6] = extra_data_from_cpu->INaCa_multiplicator;
-    }
-    else {
-        extra_par[0] = 6.8f;
-        extra_par[1] = 5.4f;
-        extra_par[2] = 138.3f;
-        extra_par[3] = 0.0;
-        extra_par[4] = 1.0f;
-        extra_par[5] = 1.0f;
-        extra_par[6] = 1.0f;
-
-        fibs = (real*) malloc(fibs_size);
-
-		for(uint64_t i = 0; i < num_cells_to_solve; i++) {
-			fibs[i] = 1.0;
-		}
-
-        deallocate = true;
-    }
-
-    check_cuda_error(cudaMalloc((void **) &extra_parameters_device, extra_parameters_size));
-    check_cuda_error(cudaMemcpy(extra_parameters_device, extra_par, extra_parameters_size, cudaMemcpyHostToDevice));
-
-    check_cuda_error(cudaMalloc((void **) &fibrosis_device, fibs_size));
+    check_cuda_error(cudaMalloc((void **) NULL, fibs_size));
     check_cuda_error(cudaMemcpy(fibrosis_device, fibs, fibs_size, cudaMemcpyHostToDevice));
 
-uint32_t *mapping = NULL;
+    uint32_t *mapping = NULL;
     uint32_t *mapping_device = NULL;
     if(ode_solver->ode_extra_data) 
     {
-	// log_info("-------------   MAPPING!!!    --------------\n\n");
         mapping = (uint32_t*)ode_solver->ode_extra_data;
         check_cuda_error(cudaMalloc((void **)&mapping_device, ode_solver->extra_data_size));
         check_cuda_error(cudaMemcpy(mapping_device, mapping, ode_solver->extra_data_size, cudaMemcpyHostToDevice));
@@ -126,17 +73,17 @@ uint32_t *mapping = NULL;
     {
         log_error_and_exit("You need to specify a mask function when using a mixed model!\n");
     }
-if(mapping_device) check_cuda_error(cudaFree(mapping_device));
 
-    solve_gpu<<<GRID, BLOCK_SIZE>>>(dt, sv, stims_currents_device, cells_to_solve_device, num_cells_to_solve, num_steps, fibrosis_device, extra_parameters_device,mapping_device);
+    solve_gpu<<<GRID, BLOCK_SIZE>>>(dt, sv, stims_currents_device, cells_to_solve_device, num_cells_to_solve, num_steps, NULL, mapping_device);
 
     check_cuda_error( cudaPeekAtLastError() );
 
     check_cuda_error(cudaFree(stims_currents_device));
-    check_cuda_error(cudaFree(fibrosis_device));
-    check_cuda_error(cudaFree(extra_parameters_device));
+    check_cuda_error(cudaFree(NULL));
+    // check_cuda_error(cudaFree(extra_parameters_device));
 
     if(cells_to_solve_device) check_cuda_error(cudaFree(cells_to_solve_device));
+    if(mapping_device) check_cuda_error(cudaFree(mapping_device));
 
     if(deallocate) free(fibs);
 }
@@ -148,25 +95,17 @@ __global__ void kernel_set_model_inital_conditions(real *sv, real*IC, int num_vo
 
     if(threadID < num_volumes) {
 
-        *((real *) ((char *) sv + pitch * 0) + threadID) = INITIAL_V;   // V;       millivolt
-        *((real *) ((char *) sv + pitch * 1) + threadID) = 0.0f; //M
-        *((real *) ((char *) sv + pitch * 2) + threadID) = 0.75; //H
-        *((real *) ((char *) sv + pitch * 3) + threadID) = 0.75; //J
-        *((real *) ((char *) sv + pitch * 4) + threadID) = 0.0f; //Xr1
-        *((real *) ((char *) sv + pitch * 5) + threadID) = 0.0f; //Xs
-        *((real *) ((char *) sv + pitch * 6) + threadID) = 1.0; //S
-        *((real *) ((char *) sv + pitch * 7) + threadID) = 1.0; //F
-        *((real *) ((char *) sv + pitch * 8) + threadID) = 1.0; //F2
-        *((real *) ((char *) sv + pitch * 9) + threadID) = 0.0; //D_INF
-        *((real *) ((char *) sv + pitch * 10) + threadID) = 0.0; //R_INF
-        *((real *) ((char *) sv + pitch * 11) + threadID) = 0.0; //Xr2_INF
+        *((real *) ((char *) sv + pitch * 0) + threadID) = 0.f;        // u
+        *((real *) ((char *) sv + pitch * 1) + threadID) = 1.f;        // v
+        *((real *) ((char *) sv + pitch * 2) + threadID) = 1.f;        // w
+        *((real *) ((char *) sv + pitch * 3) + threadID) = 0.f;        // s
     }
 }
 
 // Solving the model for each cell in the tissue matrix ni x nj
 __global__ void solve_gpu(real dt, real *sv, real* stim_currents,
                           uint32_t *cells_to_solve, uint32_t num_cells_to_solve,
-                          int num_steps, real *fibrosis, real *extra_parameters,uint32_t *mapping)
+                          int num_steps, real *fibrosis, uint32_t *mapping)
 {
     int threadID = blockDim.x * blockIdx.x + threadIdx.x;
     int sv_id;
@@ -182,23 +121,19 @@ __global__ void solve_gpu(real dt, real *sv, real* stim_currents,
 
         for (int n = 0; n < num_steps; ++n) {
 
-
-		if (mapping[sv_id] == 0) {
-			RHS_gpu(sv, rDY, stim_currents[threadID], sv_id, dt, fibrosis[threadID], extra_parameters,0);
-		}
-		else if(mapping[sv_id] == 1){
-			RHS_gpu(sv, rDY, stim_currents[threadID], sv_id, dt, fibrosis[threadID], extra_parameters,1);
-		}
-            	else {
-			RHS_gpu(sv, rDY, stim_currents[threadID], sv_id, dt, fibrosis[threadID], extra_parameters,2);
-		}
-
-
-            //RHS_gpu(sv, rDY, stim_currents[threadID], sv_id, dt, fibrosis[threadID], extra_parameters);
+            if (mapping[sv_id] == 0) {
+                RHS_gpu(sv, rDY, stim_currents[threadID], sv_id, dt, NULL, 0);
+            }
+            else if(mapping[sv_id] == 1){
+                RHS_gpu(sv, rDY, stim_currents[threadID], sv_id, dt, NULL, 1);
+            }
+            else {
+                RHS_gpu(sv, rDY, stim_currents[threadID], sv_id, dt, NULL, 2);
+            }
 
             *((real*)((char*)sv) + sv_id) = dt*rDY[0] + *((real*)((char*)sv) + sv_id);
 
-            for(int i = 1; i < NEQ; i++) {
+            for(int i = 0; i < NEQ; i++) {
                 *((real*)((char*)sv + pitch * i) + sv_id) = rDY[i];
             }
 
@@ -208,24 +143,204 @@ __global__ void solve_gpu(real dt, real *sv, real* stim_currents,
 }
 
 
-inline __device__ void RHS_gpu(real *sv_, real *rDY_, real stim_current, int threadID_, real dt, real fibrosis, real *extra_parameters, int type_cell) {
+inline __device__ void RHS_gpu(real *sv_, real *rDY_, real stim_current, int threadID_, real dt, real fibrosis, int type_cell) {
 
     //fibrosis = 0 means that the cell is fibrotic, 1 is not fibrotic. Anything between 0 and 1 means border zone
-    const real svolt = *((real*)((char*)sv_ + pitch * 0) + threadID_);
-    const real sm   = *((real*)((char*)sv_ + pitch * 1) + threadID_);
-    const real sh   = *((real*)((char*)sv_ + pitch * 2) + threadID_);
-    const real sj   = *((real*)((char*)sv_ + pitch * 3) + threadID_);
-    const real sxr1 = *((real*)((char*)sv_ + pitch * 4) + threadID_);
-    const real sxs  = *((real*)((char*)sv_ + pitch * 5) + threadID_);
-    const real ss   = *((real*)((char*)sv_ + pitch * 6) + threadID_);
-    const real sf  = *((real*)((char*)sv_ + pitch * 7) + threadID_);
-    const real sf2  = *((real*)((char*)sv_ + pitch * 8) + threadID_);
-    const real D_INF  = *((real*)((char*)sv_ + pitch * 9) + threadID_);
-    const real R_INF  = *((real*)((char*)sv_ + pitch * 10) + threadID_);
-    const real Xr2_INF  = *((real*)((char*)sv_ + pitch * 11) + threadID_);
+    const real u   = *((real*)((char*)sv_ + pitch * 0) + threadID_);
+    const real v   = *((real*)((char*)sv_ + pitch * 1) + threadID_);
+    const real w   = *((real*)((char*)sv_ + pitch * 2) + threadID_);
+    const real s   = *((real*)((char*)sv_ + pitch * 3) + threadID_);
 
-    //FUCK YOU NVCC
-    #include "tt3_mixed_common.inc.c"
+    // Constants
+    const real u_o = 0.0;
+    const real theta_v = 0.3;
+    const real theta_w = 0.13;
+    const real tau_vplus = 1.4506;
+    const real tau_s1 = 2.7342;
+    const real k_s = 2.0994;
+    const real u_s = 0.9087;
+
+    //real u_o = 0.0;
+    real u_u;
+    //real theta_v;
+    //real theta_w;
+    real theta_vminus;
+    real theta_o;
+    real tau_v1minus;
+    real tau_v2minus;
+    //real tau_vplus;
+    real tau_w1minus;
+    real tau_w2minus;
+    real k_wminus;
+    real u_wminus;
+    real tau_wplus;
+    real tau_fi;
+    real tau_o1;
+    real tau_o2;
+    real tau_so1;
+    real tau_so2;
+    real k_so;
+    real u_so;
+    //real tau_s1;
+    real tau_s2;
+    //real k_s;
+    //real u_s;
+    real tau_si;
+    real tau_winf;
+    real w_infstar;
+
+    if (type_cell == 0) {        // ENDO
+      //u_o = 0.0;
+      u_u = 1.56;
+      //theta_v = 0.3;
+      //theta_w = 0.13;
+      theta_vminus = 0.2;
+      theta_o = 0.006;
+      tau_v1minus = 75.0;
+      tau_v2minus = 10.0;
+      //tau_vplus = 1.4506;
+      tau_w1minus = 6.0;
+      tau_w2minus = 140.0;
+      k_wminus = 200.0;
+      u_wminus = 0.016;
+      tau_wplus = 280.0;
+      tau_fi = 0.1;
+      tau_o1 = 470.0;
+      tau_o2 = 6.0;
+      tau_so1 = 40.0;
+      tau_so2 = 1.2;
+      k_so = 2.0;
+      u_so = 0.65;
+      //tau_s1 = 2.7342;
+      tau_s2 = 2.0;
+      //k_s = 2.0994;
+      //u_s = 0.9087;
+      tau_si = 2.9013;
+      tau_winf = 0.0273;
+      w_infstar = 0.78;
+    }
+    else if (type_cell == 1) {   // MYO
+      //u_o = 0.0;
+      u_u = 1.61;
+      //theta_v = 0.3;
+      //theta_w = 0.13;
+      theta_vminus = 0.1;
+      theta_o = 0.005;
+      tau_v1minus = 80.0;
+      tau_v2minus = 1.4506;
+      //tau_vplus = 1.4506;
+      tau_w1minus = 70.0;
+      tau_w2minus = 8.0;
+      k_wminus = 200.0;
+      u_wminus = 0.016;
+      tau_wplus = 280.0;
+      tau_fi = 0.078;
+      tau_o1 = 410.0;
+      tau_o2 = 7.0;
+      tau_so1 = 91.0;
+      tau_so2 = 0.8;
+      k_so = 2.1;
+      u_so = 0.6;
+      //tau_s1 = 2.7342;
+      tau_s2 = 4.0;
+      //k_s = 2.0994;
+      //u_s = 0.9087;
+      tau_si = 3.3849;
+      tau_winf = 0.01;
+      w_infstar = 0.5;
+    }
+    else {                       // EPI
+      //u_o = 0.0;
+      u_u = 1.55;
+      //theta_v = 0.3;
+      //theta_w = 0.13;
+      theta_vminus = 0.006;
+      theta_o = 0.006;
+      tau_v1minus = 60.0;
+      tau_v2minus = 1150.0;
+      //tau_vplus = 1.4506;
+      tau_w1minus = 60.0;
+      tau_w2minus = 15.0;
+      k_wminus = 65.0;
+      u_wminus = 0.03;
+      tau_wplus = 200.0;
+      tau_fi = 0.11;
+      tau_o1 = 400.0;
+      tau_o2 = 6.0;
+      tau_so1 = 30.0181;
+      tau_so2 = 0.9957;
+      k_so = 2.0458;
+      u_so = 0.65;
+      //tau_s1 = 2.7342;
+      tau_s2 = 16.0;
+      //k_s = 2.0994;
+      //u_s = 0.9087;
+      tau_si = 1.8875;
+      tau_winf = 0.07;
+      w_infstar = 0.94;
+    }
+
+        // Get du_dt, dv_dt, dw_dt and ds_dt
+        //du_dt = - reaction_u(ustep, vstep, wstep, sstep) + I_stim;
+        //
+        real H = 0.0;
+        real h_o = 0.0;
+        real h_w = 0.0;
+        real h_v_minus = 0.0;
+        real tau_vminus = 0.0;
+        real J_fi = 0.0;
+        real J_so = 0.0;
+        real J_si = 0.0;
+        real tau_o = 0.0;
+        real tau_so = 0.0;
+        real tau_s = 0.0;
+        // real du_dt = 0.0;
+        // real dv_dt = 0.0;
+        // real dw_dt = 0.0;
+        // real ds_dt = 0.0;
+        real v_inf = 0.0;
+        real w_inf = 0.0;
+        real tau_wminus = 0.0;
+        real I_stim = stim_current;
+
+        if (u-theta_v > 0)
+          H = 1.0;
+        J_fi = -v * H * (u-theta_v) * (u_u-u) / tau_fi;
+        if (u-theta_o > 0)
+          h_o = 1.0;
+        tau_o = (1.0 - h_o) * tau_o1 + (h_o * tau_o2);
+        if (u-theta_w > 0)
+          h_w = 1.0;
+        tau_so = tau_so1 + (((tau_so2 - tau_so1) * (1.0 + tanh(k_so*(u - u_so)))) * 0.5);
+        J_so = ((u-u_o) * (1.0 - h_w) / tau_o) + (h_w / tau_so);
+        J_si = - h_w * w * s / tau_si;
+        rDY_[0] = - (J_fi + J_so + J_si) + I_stim;
+        //
+
+        //dv_dt = dvdt(ustep, vstep);
+        //
+        if (u < theta_vminus)
+        {
+            v_inf = 1.0;
+        }
+        if (u-theta_vminus > 0)
+          h_v_minus = 1.0;
+        tau_vminus = (1.0 - h_v_minus) * tau_v1minus + (h_v_minus * tau_v2minus);
+        rDY_[1] = (1.0 - H) * (v_inf - v) / tau_vminus - (H * v / tau_vplus);
+        //
+
+        //dw_dt = dwdt(ustep, wstep);
+        //
+        w_inf = (1.0 - h_o) * (1.0 - (u/tau_winf)) + (h_o * w_infstar);
+        tau_wminus = tau_w1minus + (((tau_w2minus - tau_w1minus) * (1.0 + tanh(k_wminus*(u - u_wminus)))) * 0.5);
+        rDY_[2] = (1.0 - h_w) * (w_inf - w) / tau_wminus - (h_w * w / tau_wplus);
+        //
+
+        //ds_dt = dsdt(ustep, sstep);
+        //
+        tau_s = (1.0 - h_w) * tau_s1 + (h_w * tau_s2);
+        rDY_[3] = (((1.0 + tanh(k_s*(u - u_s))) * 0.5) - s) / tau_s;
+        //
 
 
 }
--- src/models_library/minimal_model/minimal_model.h
+++ src/models_library/minimal_model/minimal_model.h
@@ -17,10 +17,10 @@ static size_t pitch_h;
 __global__ void kernel_set_model_inital_conditions(real *sv, real* ICs, int num_volumes);
 
 __global__ void solve_gpu(real dt, real *sv, real *stim_currents, uint32_t *cells_to_solve, uint32_t num_cells_to_solve,
-                          int num_steps, real *fibrosis, real *extra_parameters,uint32_t *mapping);
+                          int num_steps, real *fibrosis,uint32_t *mapping);
 
 inline __device__ void RHS_gpu(real *sv_, real *rDY_, real stim_current, int threadID_, real dt, real fibrosis,
-                               real *extra_parameters, int type_cell);
+                                int type_cell);
 
 #endif
 
